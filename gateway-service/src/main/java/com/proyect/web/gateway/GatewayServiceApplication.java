bdf9ceda8d874056faf71189abd737c3aa0a37c1
PieroCahuanaPalomino <piero.cahuana.69.palomino@gmail.com>
2022-08-01 15:37:34 -0500

    keycloak

--- gateway-service/src/main/java/com/proyect/web/gateway/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/proyect/web/gateway/GatewayServiceApplication.java
@@ -3,6 +3,12 @@ package com.proyect.web.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -11,5 +17,6 @@ public class GatewayServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
+
 
 }

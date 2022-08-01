8b532e2e0ecc5ef6e8359ca3024d57d71c7849be
PieroCahuanaPalomino <piero.cahuana.69.palomino@gmail.com>
2022-08-01 15:04:18 -0500

    keycloak

--- gateway-service/src/main/java/com/proyect/web/gateway/security/CORS.java
+++ gateway-service/src/main/java/com/proyect/web/gateway/security/CORS.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.proyect.web.product.security;
+package com.proyect.web.gateway.security;
 
 import java.io.IOException;
 

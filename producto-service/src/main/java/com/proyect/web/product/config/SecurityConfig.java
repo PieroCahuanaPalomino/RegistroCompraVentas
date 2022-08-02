3fe28a4d486f3fb3db9b27fb30233fa62cd1b857
PieroCahuanaPalomino <piero.cahuana.69.palomino@gmail.com>
2022-08-02 15:39:17 -0500

    secutiry

--- producto-service/src/main/java/com/proyect/web/product/config/SecurityConfig.java
+++ producto-service/src/main/java/com/proyect/web/product/config/SecurityConfig.java
@@ -18,6 +18,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().configurationSource(corsConfigurationSource());
+        http.cors();
     }
     @Bean
     CorsConfigurationSource corsConfigurationSource() {

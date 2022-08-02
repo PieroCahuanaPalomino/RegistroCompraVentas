6d99a4d56f2d1d4ee780dbd7b852da7129be469e
PieroCahuanaPalomino <piero.cahuana.69.palomino@gmail.com>
2022-08-02 09:57:00 -0500

    keycloak

--- gateway-service/src/main/java/com/proyect/web/gateway/security/CORS.java
+++ gateway-service/src/main/java/com/proyect/web/gateway/security/CORS.java
@@ -20,51 +20,46 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
-@Component
-@Order(Ordered.HIGHEST_PRECEDENCE)
-public class CORS implements Filter {
 
-	@Value("${cors.Access-Control-Allow-Origin}")
-	private String allowOrigin;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.cors.reactive.CorsUtils;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
 
-	@Value("${cors.Access-Control-Allow-Methods}")
-	private String allowMethods;
+@Configuration
+public class CORS {
 
-	@Value("${cors.Access-Control-Max-Age}")
-	private String maxAge;
+	  private static final String ALLOWED_HEADERS = "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN, token, username, client";
+	    private static final String ALLOWED_METHODS = "*";
+	    private static final String ALLOWED_ORIGIN  = "*";
+	    private static final String ALLOWED_EXPOSE  = "*";
 
-	@Value("${cors.Access-Control-Allow-Headers}")
-	private String allowHeaders;
-
-	@Value("${cors.Access-Control-Allow-Credentials}")
-	private String allowCredentials;
-
-	@Override
-	public void init(FilterConfig filterConfig) throws ServletException {
-	}
-
-	@Override
-	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
-			throws IOException, ServletException {
-
-		HttpServletResponse response = (HttpServletResponse) res;
-		HttpServletRequest request = (HttpServletRequest) req;
-
-		response.addHeader("Access-Control-Allow-Origin", allowOrigin);
-		response.addHeader("Access-Control-Allow-Methods", allowMethods);
-		response.addHeader("Access-Control-Max-Age", maxAge);
-		response.addHeader("Access-Control-Allow-Headers", allowHeaders);
-		response.addHeader("Access-Control-Allow-Credentials", allowCredentials);
-
-		if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
-			response.setStatus(HttpServletResponse.SC_OK);
-		} else {
-			chain.doFilter(req, res);
-		}
-	}
-
-	@Override
-	public void destroy() {
-	}
-
+	    @Bean
+	    public WebFilter corsFilter() {
+	        return (ServerWebExchange ctx, WebFilterChain chain) -> {
+	            ServerHttpRequest request = ctx.getRequest();
+	            if (CorsUtils.isCorsRequest(request)) {
+	                ServerHttpResponse response = ctx.getResponse();
+	                HttpHeaders headers = response.getHeaders();
+	                headers.add("Access-Control-Allow-Origin", ALLOWED_ORIGIN);
+	                headers.add("Access-Control-Allow-Methods", ALLOWED_METHODS);
+	                headers.add("Access-Control-Allow-Headers", ALLOWED_HEADERS);
+	                headers.add("Access-Control-Expose-Headers", ALLOWED_EXPOSE);
+	                headers.add("Access-Control-Allow-Credentials", "true");
+	                if (request.getMethod() == HttpMethod.OPTIONS) {
+	                    response.setStatusCode(HttpStatus.OK);
+	                    return Mono.empty();
+	                }
+	            }
+	            return chain.filter(ctx);
+	        };
+	    }
 }

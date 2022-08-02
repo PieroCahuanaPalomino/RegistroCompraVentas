612b1d404d08b2d52110fd6fa626df2cd01f7ac4
PieroCahuanaPalomino <piero.cahuana.69.palomino@gmail.com>
2022-08-02 14:36:31 -0500

    origins

--- producto-service/src/main/java/com/proyect/web/product/controller/ProductoController.java
+++ producto-service/src/main/java/com/proyect/web/product/controller/ProductoController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -55,6 +56,7 @@ public class ProductoController {
 	
 	//se enviaran por el DTO todos los datos
 	@GetMapping()
+	@CrossOrigin(origins = "*")
 	public ResponseEntity <List<ProductoDto>> listar(){
 		List<ProductoDto> productosDto=productoService.listaProducto();
 		if(productosDto.isEmpty()) {return ResponseEntity.noContent().build();}

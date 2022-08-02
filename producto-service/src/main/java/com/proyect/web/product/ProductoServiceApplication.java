42db1d6cf8ef407e7db777e795042bed3674fb91
PieroCahuanaPalomino <piero.cahuana.69.palomino@gmail.com>
2022-08-02 13:24:15 -0500

    mvc configurer

--- producto-service/src/main/java/com/proyect/web/product/ProductoServiceApplication.java
+++ producto-service/src/main/java/com/proyect/web/product/ProductoServiceApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 @EnableEurekaClient
 public class ProductoServiceApplication {
@@ -14,4 +19,14 @@ public class ProductoServiceApplication {
 		SpringApplication.run(ProductoServiceApplication.class, args);
 	}
 
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*").allowedHeaders("*");
+			}
+		};
+	}
 }
